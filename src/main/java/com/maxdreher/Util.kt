a416d3bdc6d2c31771d28b45fc0e6499e46e4d8f
Max Dreher <maxdmax101@gmail.com>
2021-03-25 19:29:03 -0400

    Add Util

--- src/main/java/com/maxdreher/Util.kt
+++ src/main/java/com/maxdreher/Util.kt
@@ -1,12 +1,24 @@
 package com.maxdreher
 
 import android.content.Context
+import android.view.View
+import android.widget.Button
+import androidx.navigation.findNavController
 
 class Util {
     companion object {
         fun getDefaultMargin(context: Context, id: Int): Int {
             with(context) {
                 return resources.getDimension(id).toInt()
+            }
+        }
+
+        fun buttonToAction(view: View, map: Map<Int, Int>) {
+            val navy = view.findNavController()
+            map.entries.forEach { entry ->
+                view.findViewById<Button>(entry.key).setOnClickListener {
+                    navy.navigate(entry.value)
+                }
             }
         }
     }
