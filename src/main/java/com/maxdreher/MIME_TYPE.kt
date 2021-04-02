4b0b73dea15606ce1df2d64fbd4e371b88e08796
Max Dreher <maxdmax101@gmail.com>
2021-04-02 14:47:12 -0400

    Add MIME_TYPE

---
+++ src/main/java/com/maxdreher/MIME_TYPE.kt
@@ -0,0 +1,22 @@
+package com.maxdreher
+
+enum class MIME_TYPE(val value: String) {
+    HTML("text/html"),
+    HTML_ZIPPED("application/zipapplication/zip"),
+    PLAIN_TEXT("text/plaintext/plain"),
+    RICH_TEXT("application/rtf"),
+    OPEN_OFFICE_DOC("application/vnd.oasis.opendocument.text"),
+    PDF("application/pdfapplication/pdfapplication/pdfapplication/pdf"),
+    MS_WORD_DOCUMENT("application/vnd.openxmlformats-officedocument.wordprocessingml.document"),
+    EPUB("application/epub+zip"),
+    MS_EXCEL("application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"),
+    OPEN_OFFICE_SHEET("application/x-vnd.oasis.opendocument.spreadsheet"),
+    CSV("text/csv"),
+    SHEET_ONLY("text/tab-separated-values"),
+    JPEG("image/jpeg"),
+    PNG("image/png"),
+    SVG("image/svg+xml"),
+    MS_POWERPOINT("application/vnd.openxmlformats-officedocument.presentationml.presentation"),
+    OPEN_OFFICE_PRESENTATION("application/vnd.oasis.opendocument.presentation"),
+    JSON("application/vnd.google-apps.script+json");
+}
