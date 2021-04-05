0c93f386020322685382ad150928bcf04acdaff8
Max Dreher <maxdmax101@gmail.com>
2021-04-05 16:12:14 -0400

    Add resource ID to ActivityBase

--- src/main/java/com/maxdreher/extensions/ActivityBase.kt
+++ src/main/java/com/maxdreher/extensions/ActivityBase.kt
@@ -1,8 +1,10 @@
 package com.maxdreher.extensions
 
 import android.content.Context
+import androidx.annotation.LayoutRes
 import androidx.appcompat.app.AppCompatActivity
 
-open class ActivityBase : AppCompatActivity(), IContextBase {
+open class ActivityBase(@LayoutRes contentLayoutId: Int) : AppCompatActivity(contentLayoutId),
+    IContextBase {
     override fun getContext(): Context? = applicationContext
 }
